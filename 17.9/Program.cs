d3b3752a6761f40c9fffc0c513fe6d22b726ed27
viktorkroumi <145652274+viktorkroumi@users.noreply.github.com>
2024-09-17 09:27:09 +0200

    fixes #4

--- 17.9/Program.cs
+++ 17.9/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-Console.WriteLine("Text now has a meaning");
+Console.WriteLine("Text has a meaning");
 
-Console.WriteLine("Text now has a meaning");
+Console.WriteLine("Text has a meaning");
